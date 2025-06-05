b36ce77cd629c5c8bdab4829da975ab200630921
alap2s <alapshah.com@gmail.com>
2025-06-05 11:21:16 +0200

    security: use environment variables for Firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDn2hgT-cYoGUQpFdI-sXsb4I2dDSJq-FY",
-  authDomain: "muuappberlin.firebaseapp.com",
-  projectId: "muuappberlin",
-  storageBucket: "muuappberlin.appspot.com",
-  messagingSenderId: "820793338754",
-  appId: "1:820793338754:web:a9552c7b99df3203107d9b",
-  measurementId: "G-LHJTKXWG1W"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
